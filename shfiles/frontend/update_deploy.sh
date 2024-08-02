57c8d295a62fb8e0fc548db7c13ea0080c892343
Abhay Ramgarhia <abhay.ramgarhia@shubpy.com>
2024-08-02 22:48:22 +0530

    34

--- shfiles/frontend/update_deploy.sh
+++ shfiles/frontend/update_deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 kubectl rollout restart deployment \
+    ailyze-companion-deployment \--namespace uppy \
     ailyze-react-deployment \
-    ailyze-companion-deployment -n uppy \
+    
     "$@"
