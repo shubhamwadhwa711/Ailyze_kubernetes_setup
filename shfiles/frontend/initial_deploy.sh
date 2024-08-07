b15005ed0ed31e580b3dddcb34742f4122ccdef6
Abhay Ramgarhia <abhay.ramgarhia@shubpy.com>
2024-08-07 22:39:33 +0530

    45

--- shfiles/frontend/initial_deploy.sh
+++ shfiles/frontend/initial_deploy.sh
@@ -4,5 +4,6 @@ kubectl apply \
     -f ./frontend/ailyze_react.yaml \
     -f ./frontend/ailyze_companion.yaml \
     -f ./volumes/ailyze_companion_volume.yaml \
-    -f ./ingress/ailyze_companion_ingress.yaml 
+    -f ./ingress/ailyze_companion_ingress.yaml \
+    -f ./ingress/ailyze_react_ingress.yaml \
     "$@";
