74bc6086a7e389e934237ca718458f3185bddbd0
abhayramgarhia@gmail.com <abhayramgarhia@gmail.com>
2024-08-30 00:27:22 +0530

    #4

--- shfiles/backend/update_manifest_deploy.sh
+++ shfiles/backend/update_manifest_deploy.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Function to print messages
+print_message() {
+    echo "===== $1 ====="
+}
+
+# Updating Deployments
+print_message "Updating Deployments"
 kubectl rollout restart deployment \
     ailyze-django-deployment \
     ailyze-django-bot-deployment \
@@ -8,5 +19,11 @@ kubectl rollout restart deployment \
     ailyze-celery-bot-deployment \
     ailyze-flower-deployment \
     ailyze-redis-deployment \
-    ailyze-rabbitmq-deployment
     "$@"
+
+# Updating Statefulsets
+print_message "Updating Statefulsets"
+kubectl rollout restart statefulset \
+    ailyze-rabbitmq-statefulset
+
+print_message "Rollout restart completed"
