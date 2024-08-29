5c479630823b41024032cba79200a525e922dc66
abhayramgarhia@gmail.com <abhayramgarhia@gmail.com>
2024-08-29 14:06:39 +0530

    3

--- shfiles/backend/intialize_manifest_deploy.sh
+++ shfiles/backend/intialize_manifest_deploy.sh
@@ -4,10 +4,10 @@ kubectl apply \
     -f ./secrets/ailyze_db_secrets.yaml \
     -f ./secrets/acr_secret.yaml \
     -f ./volumes/ailyze_celery_volume.yaml \
+    -f ./deployments/ailyze_rabbitmq.yaml \
     -f ./deployments/ailyze_django.yaml \
     -f ./deployments/ailyze_django_bot.yaml \
     -f ./deployments/ailyze_redis.yaml \
-    -f ./deployments/ailyze_rabbitmq.yaml \
     -f ./deployments/ailyze_flower.yaml \
     -f ./deployments/ailyze_celery_bot.yaml \
     -f ./deployments/ailyze_celery_theme.yaml \
